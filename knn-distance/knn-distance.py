e6914078541f12b57e62c7b78ee83989fa397052
林楷瀚 <117831604+kenneth-lin-2003@users.noreply.github.com>
2026-06-13 21:34:31 +0800

    Solve knn-distance

--- knn-distance/knn-distance.py
+++ knn-distance/knn-distance.py
@@ -5,20 +5,17 @@ def knn_distance(X_train, X_test, k):
     Compute pairwise distances and return k nearest neighbor indices.
     """
     # Write code here
-    X_train = np.array(X_train)
-    X_test = np.array(X_test)
-    if X_train.ndim == 1:
-        X_train = X_train[:, np.newaxis]
+    X_train, X_test = np.array(X_train), np.array(X_test)
     if X_test.ndim == 1:
-        X_test = X_test[:, np.newaxis]
-    _X_train = np.sum(np.square(X_train), axis=-1, keepdims=True)
-    _X_test = np.sum(np.square(X_test), axis=-1, keepdims=True)
-    _X_train = _X_train.transpose(1, 0)
-    Q = _X_test + _X_train - 2 * (X_test @ X_train.T)
-    idx = np.argsort(Q, axis = -1)
-    if k > X_train.shape[0]:
-        ret = np.full((X_test.shape[0], k), -1)
-        ret[:, :X_train.shape[0]] = idx
-        return ret
-    return idx[:,:k]
-    pass
+        x = np.abs(X_test[:,None] - X_train[None,:])
+        x = np.argsort(x, axis=-1)[:,:k]
+        pad_len = max(0, k-x.shape[1])
+        x = np.pad(x, ((0,0),(0,pad_len)), mode="constant", constant_values=-1)
+        return x
+    dtrain, dtest = X_train.shape[0], X_test.shape[0]
+    x = X_test.reshape(dtest, 1, -1) - X_train.reshape(1, dtrain, -1)
+    x = np.sum(np.square(x), axis=-1)
+    x = np.argsort(x, axis=-1)[:,:k]
+    pad_len = max(0, k-x.shape[1])
+    x = np.pad(x, ((0,0),(0,pad_len)), mode="constant", constant_values=-1)
+    return x
